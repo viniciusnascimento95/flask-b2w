2c1312cb834cf0c180cb132c19c669acfe6f0ca2
nascimento vinicius <nascimento.vinicius32@gmail.com>
2019-07-28 19:46:25 -0400

    docs with postman json

--- tests/home/test_resource.py
+++
@@ -1,8 +0,0 @@
-# import json
-
-# def test_home_response_hello(client):
-#     response = client.get('/')
-
-#     data = json.loads(response.data.decode('utf-8'))
-
-#     assert data['hello'] == 'world by apps'
